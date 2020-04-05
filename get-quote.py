71584f846337a4ce722398b5f34eb4b2d0a224f2
infinitEnigma <yole71@gmail.com>
2020-04-05 22:32:08 +0200

    add some philosophy quotes

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,15 @@
-import random
+import random, sys
 def primary():
-  #  print("Keep it logically awesome.")
+  #  open txt file to read quotes
 
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
+
+    # print random quote
     last = len(quotes) - 1
     rnd = random.randint(0, last)
-    print(quotes[rnd])
-
+    print(quotes[rnd], end="")
+    
 if __name__== "__main__":
     primary()
